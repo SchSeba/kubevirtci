7304c7cbf076a2d68180d6b748ca19c0721d71d9
Sebastian Sch <sebassch@gmail.com>
2019-10-03 14:15:37 +0300

    WIP
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- cluster-provision/k8s/multus-1.15.1/provision.sh
+++ cluster-provision/k8s/multus-1.15.1/provision.sh
@@ -5,4 +5,6 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export version=1.15.1
-../publish.sh
+export extra="multus-"
+export extraArgs="--network-operator"
+../provision.sh
---
+++ cluster-provision/okd/network-4.1/provision.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
+
+okd_base_hash="sha256:259e776998da3a503a30fdf935b29102443b24ca4ea095c9478c37e994e242bb"
+gocli_image_hash="sha256:a7880757e2d2755c6a784c1b64c64b096769ed3ccfac9d8e535df481731c2144"
+
+gocli="docker run \
+--privileged \
+--net=host \
+--rm -t \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v ${PARENT_DIR}:${PARENT_DIR} \
+docker.io/kubevirtci/gocli@${gocli_image_hash}"
+
+${gocli} provision okd \
+--prefix okd-network-4.1-provision \
+--dir-scripts ${PARENT_DIR}/okd/scripts \
+--dir-manifests ${PARENT_DIR}/manifests \
+--dir-hacks ${PARENT_DIR}/okd/hacks \
+--master-memory 10240 \
+--installer-pull-token-file ${INSTALLER_PULL_SECRET} \
+--installer-repo-tag release-4.1 \
+--installer-release-image docker.io/kubevirtci/ocp-release:4.1.15 \
+"kubevirtci/okd-base@${okd_base_hash}"
